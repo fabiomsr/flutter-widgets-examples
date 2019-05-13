b50665f7a95dd47f0a1628615547f06ef7a55dae
Chema Rubio <chemax@gmail.com>
2019-05-13 19:45:09 +0000

    ADD: CustomPaint reference

--- README.md
+++ README.md
@@ -5,8 +5,12 @@
 |------------------------------|----------------------------------------------------------------------|---------|
 | **Animation and motion widgets** |                                                                      |         |
 | AnimatedContainer            | [AnimatedContainer][Doc_AnimatedContainer] | [Source][Source_AnimatedContainer]      |
+| **Painting and effects** |                                                                      |         |
+| CustomPaint            | [CustomPaint][Doc_CustomPaint] | [Source][Source_CustomPaint]      |
 
 
 [Doc_AnimatedContainer]:https://api.flutter.dev/flutter/widgets/AnimatedContainer-class.html
+[Doc_CustomPaint]:https://api.flutter.dev/flutter/widgets/CustomPaint-class.html
 
 [Source_AnimatedContainer]:/lib/animation_and_motion_widgets/animated_container.dart
+[Source_CustomPaint]:/lib/painting_and_effects/animated_container.dart
---
+++ lib/painting_and_effects/custom_paint.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+void main() => runApp(CustomPaint());
+
+class CustomPaintApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Paint',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: CustomPaintExample(),
+    );
+  }
+}
+
+class CustomPaintExample extends StatefulWidget {
+  CustomPaintExample({Key key}) : super(key: key);
+
+  @override
+  _CustomPaintExampleState createState() => _CustomPaintExampleState();
+}
+
+class _CustomPaintExampleState extends State<CustomPaintExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: CustomPaint(
+        painter: FaceEmotionPainter(),
+      ),
+    );
+  }
+}
+
+class FaceEmotionPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = Math.min(size.width, size.height) / 2;
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final Paint backgroundPaint = new Paint()..style = PaintingStyle.fill;
+    backgroundPaint..color = Colors.green;
+
+    final Paint facePaint = new Paint()
+      ..isAntiAlias = true
+      ..strokeWidth = 10.0
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke;
+
+    // Draw Container
+    canvas.drawRect(
+        Rect.fromCircle(center: center, radius: radius / 0.9), backgroundPaint);
+
+    // Draw mouth
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius / 4), 0,
+        Math.pi, false, facePaint);
+
+    // Draw face
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radius), 10.0, 20.0,
+        false, facePaint);
+
+    // Draw eyes
+    canvas.drawCircle(
+        Offset(center.dx - radius / 2, center.dy - radius / 4), 2, facePaint);
+    canvas.drawCircle(
+        Offset(center.dx + radius / 2, center.dy - radius / 4), 2, facePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
