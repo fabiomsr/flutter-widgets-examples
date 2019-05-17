419d8fa4d3e4f05d8a93224d9872064cc6bba8eb
Chema Rubio <chemax@gmail.com>
2019-05-17 17:17:15 +0000

    feat: add drawer example

---
+++ lib/material_widgets/drawer.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DrawerExampleApp());
+
+class DrawerExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DrawerExample(),
+    );
+  }
+}
+
+class DrawerExample extends StatefulWidget {
+  DrawerExample({Key key}) : super(key: key);
+
+  @override
+  _DrawerExampleState createState() => _DrawerExampleState();
+}
+
+class _DrawerExampleState extends State<DrawerExample> {
+  final backgroundColors = [
+    Colors.green,
+    Colors.purple,
+    Colors.blue,
+    Colors.orange
+  ];
+
+  String _name = 'Max Power';
+  String _mail = 'max.power@mail.com';
+
+  int selectedItem = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColors[selectedItem],
+      appBar: AppBar(),
+      body: Center(
+        child: Text("Drawer Example"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text(_mail),
+              accountName: Text(_name),
+            ),
+            ListTile(
+              title: Text("Green"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                setState(() {
+                  selectedItem = 0;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Purple"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                setState(() {
+                  selectedItem = 1;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Blue"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                setState(() {
+                  selectedItem = 2;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Orange"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                setState(() {
+                  selectedItem = 3;
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
