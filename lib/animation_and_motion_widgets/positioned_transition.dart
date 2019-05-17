5a13067435ae1b9d18ea45196d9c31d80ead47ee
fabiomsr <fabiomarcos.sr@gmail.com>
2019-05-17 21:17:58 +0200

    apply format to positioned example

--- lib/animation_and_motion_widgets/positioned_transition.dart
+++ lib/animation_and_motion_widgets/positioned_transition.dart
@@ -28,7 +28,6 @@ class PositionedTransitionExample extends StatefulWidget {
 class _PositionedTransitionExampleState
     extends State<PositionedTransitionExample>
     with SingleTickerProviderStateMixin {
-
   AnimationController _animationController;
   Animation<RelativeRect> _animation;
 
@@ -51,7 +50,8 @@ class _PositionedTransitionExampleState
     _animation = new RelativeRectTween(
       begin: new RelativeRect.fromLTRB(75.0, 400.0, 75.0, 100.0),
       end: new RelativeRect.fromLTRB(150.0, 100.0, 150.0, 400.0),
-    ).animate(CurvedAnimation(parent: _animationController, curve: Curves.elasticInOut));
+    ).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.elasticInOut));
   }
 
   @override
@@ -60,15 +60,14 @@ class _PositionedTransitionExampleState
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Stack(
-        fit: StackFit.passthrough,
-          alignment: Alignment.center,
-          children: <Widget>[
-            PositionedTransition(
-              rect: _animation,
-              child: Container(color: Colors.red,),
-            )
-          ] ),
+      body: Stack(alignment: Alignment.center, children: <Widget>[
+        PositionedTransition(
+          rect: _animation,
+          child: Container(
+            color: Colors.red,
+          ),
+        )
+      ]),
     );
   }
 }
